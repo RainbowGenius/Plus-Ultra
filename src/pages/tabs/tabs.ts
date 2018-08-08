776b4e78f622fc214caebc153e45330ab57e31fb
Bira Chamma <birachamma@outlook.com>
2018-08-08 12:03:04 -0300

    fix: remove tab navegation to IntroPage

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,7 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { FeedPage } from '../feed/feed';
-import { IntroPage } from '../intro/intro';
+// import { IntroPage } from '../intro/intro';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -15,7 +15,7 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
   tab4Root = FeedPage;
-  tab5Root = IntroPage;
+  // tab5Root = IntroPage;
 
   constructor() {
 
